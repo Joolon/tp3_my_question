2eb4ab6d3a775ff0c6264cc561d2eb850aace5f0
Joolon <179777072@qq.com>
2018-11-15 23:40:52 +0800

    部署代码到ALI云服务器

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -6,10 +6,10 @@ return array(
 
     /* 数据库配置 */
     'DB_NAME'               =>  'questionnaire',  // 数据库名
-    'DB_USER'               =>  'questionnaire',  // 用户名
-    'DB_PWD'                =>  '1qaz2wsx3edc', // 密码
+    'DB_USER'               =>  'admin',  // 用户名
+    'DB_PWD'                =>  'admin.Jolon.123456', // 密码
     'DB_TYPE'               =>  'mysql',  // 数据库类型
-    'DB_HOST'               =>  'localhost',  // 服务器地址
+    'DB_HOST'               =>  '47.107.183.46',  // 服务器地址
     'DB_PORT'               =>  '3306',  // 端口
     'DB_CHARSET'            =>  'utf8',  // 数据库编码默认采用utf8
     'DB_PREFIX'             =>  '',  // 数据库表前缀
