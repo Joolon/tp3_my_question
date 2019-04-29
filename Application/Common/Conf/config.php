4505db2db11d616b67603eab757e7f85a0bc20cc
Joolon <179777072@qq.com>
2019-04-29 14:37:59 +0800

    change db driver from mysql to mysqli

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -7,10 +7,10 @@ return array(
     /* 数据库配置 */
     'DB_USER'               =>  'root',  // 用户名
     'DB_PWD'                =>  'root.Jolon.123456', // 密码
-    'DB_HOST'               =>  '127.0.0.1',  // 服务器地址
+    'DB_HOST'               =>  '47.107.183.46',  // 服务器地址
 
     'DB_NAME'               =>  'questionnaire',  // 数据库名
-    'DB_TYPE'               =>  'mysql',  // 数据库类型
+    'DB_TYPE'               =>  'mysqli',  // 数据库类型
     'DB_PORT'               =>  '3306',  // 端口
     'DB_CHARSET'            =>  'utf8',  // 数据库编码默认采用utf8
     'DB_PREFIX'             =>  '',  // 数据库表前缀
